acaf78b10bdf66bf60ed4a9971e5fd2b941b4989
Fraviuz <gabriel.pla12@gmail.com>
2022-06-30 15:31:59 -0300

    Actividad 5 invocar error creado

---
+++ Actividad de Python 04/AP 04 - 05.py
@@ -0,0 +1,16 @@
+#05 Realiza una función llamada agregar_una_vez(lista, el) que reciba una lista y un elemento. La función debe añadir el elemento al final de la lista con la condición de no repetir ningún elemento. Además si este elemento ya se encuentra en la lista se debe invocar un error de tipo ValueError que debes capturar y mostrar este mensaje en su lugar: Error: Imposible añadir elementos duplicados => [elemento].
+
+
+def agregar_una_vez(lista, el):
+    try:
+        lista.index(el)
+        print(f'Error: Imposible añadir elementos duplicados => [{el}]')
+    except ValueError:
+        lista.append(el)
+
+
+lista = ['test', 2]
+agregar_una_vez(lista, 3)
+agregar_una_vez(lista, 'testy')
+agregar_una_vez(lista, 2)
+print(lista)
